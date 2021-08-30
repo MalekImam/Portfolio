1088f7f168d6cd7a515e7ede9c585f9e35519012
Malek <malekalemam20@gmail.com>
2021-08-30 15:18:21 +0300

    Basic layout structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import "styles/layout.css"
 import "styles/globals.css"
+import Header from "@/layout/Header"
+import Footer from "@/layout/Footer"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
